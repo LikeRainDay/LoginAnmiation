2a313d58ce25aac9c1cf3f822f4867be1ebe36d2
HouShuai <houshuai0816@126.com>
2016-07-28 23:07:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/houshuai/loginanmiation/JellyInterpolator.java
@@ -0,0 +1,19 @@
+package com.example.houshuai.loginanmiation;
+
+import android.view.animation.LinearInterpolator;
+
+//自定义的一个差值器
+public class JellyInterpolator extends LinearInterpolator {
+    private float factor;
+
+    public JellyInterpolator() {
+        this.factor = 0.15f;
+    }
+
+    @Override
+    public float getInterpolation(float input) {
+        //返回一个差值器的值
+        return (float) (Math.pow(2, -10 * input)
+                * Math.sin((input - factor / 4) * (2 * Math.PI) / factor) + 1);
+    }
+}
---
+++ app/src/main/java/com/example/houshuai/loginanmiation/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.houshuai.loginanmiation;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private TextView mBtnLogin;
+
+    private View progress;
+
+    private View mInputLayout;
+
+    private float mWidth, mHeight;
+
+    private LinearLayout mName, mPsw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBtnLogin = (TextView) findViewById(R.id.main_btn_login);
+        progress = findViewById(R.id.layout_progress);
+        mInputLayout = findViewById(R.id.input_layout);
+        mName = (LinearLayout) findViewById(R.id.input_layout_name);
+        mPsw = (LinearLayout) findViewById(R.id.input_layout_psw);
+
+        mBtnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //测量登录按钮的长宽
+        mWidth = mBtnLogin.getMeasuredWidth();
+        mHeight = mBtnLogin.getMeasuredHeight();
+
+//        mName.setVisibility(View.INVISIBLE);
+//        mPsw.setVisibility(View.INVISIBLE);
+
+        inputAnimator(mInputLayout, mWidth, mHeight);
+
+    }
+
+    private void inputAnimator(final View view, float w, float h) {
+        //动画集
+        AnimatorSet set = new AnimatorSet();
+
+        ValueAnimator animator = ValueAnimator.ofFloat(0, w);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (Float) animation.getAnimatedValue();
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) view
+                        .getLayoutParams();
+                params.leftMargin = (int) value;
+                params.rightMargin = (int) value;
+                view.setLayoutParams(params);
+            }
+        });
+
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mInputLayout,
+                "scaleX", 1f, 0.5f);
+        set.setDuration(1000);
+        set.setInterpolator(new AccelerateDecelerateInterpolator());
+        set.playTogether(animator,animator2);
+        set.start();
+        set.addListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                progress.setVisibility(View.VISIBLE);
+                progressAnimator(progress);
+                mInputLayout.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+        });
+
+    }
+    //progress属性动画
+    private void progressAnimator(final View view) {
+        PropertyValuesHolder animator = PropertyValuesHolder.ofFloat("scaleX",
+                0.5f, 1f);
+        PropertyValuesHolder animator2 = PropertyValuesHolder.ofFloat("scaleY",
+                0.5f, 1f);
+         ObjectAnimator animator3 = ObjectAnimator.ofPropertyValuesHolder(view,
+                animator, animator2);
+        animator3.setDuration(1000);
+        animator3.setInterpolator(new JellyInterpolator());
+        animator3.start();
+        animator3.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+             
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+    }
+}
